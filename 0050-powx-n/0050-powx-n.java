b717cdf785322fae634d16a0227ecef4f221740d
aniketchavan6081 <123833092+aniketchavan6081@users.noreply.github.com>
2023-07-13 00:25:04 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (86.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        // return Math.pow(x,n);
+        
+        if(n==0) return 1; // if pow is 0 return 1
+        if(n==1) return x; // if pow is 1 return that number
+        if(n==-1) return 1/x; // if pow is then return 1/x;
+
+        double ans = myPow(x,n/2); // recursive call by passing half power
+       
+        if(n>0)
+            return n%2==0 ? ans*ans : ans*ans*x;
+        else 
+            return n%2==0 ? ans*ans : ans*ans/x;
     }
 }
