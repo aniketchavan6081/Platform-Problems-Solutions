d4cbe45427272e91b9ffa39e1ab182affaa1a741
aniketchavan6081 <123833092+aniketchavan6081@users.noreply.github.com>
2023-07-12 11:50:00 +0530

    Added solution - LeetHub

--- Pattern 22 - GFG/pattern-22.java
+++ Pattern 22 - GFG/pattern-22.java
@@ -22,17 +22,27 @@ class Solution {
 
     void printSquare(int n) {
         // code here
-      for(int i=0;i<2*n-1;i++){
-          for(int j=0;j<2*n-1;j++){
-             int top = i;
-             int bottom = j;
-             int right = (2*n - 2) - j;
-             int left = (2*n - 2) - i;
-             
-             System.out.print(n- Math.min(Math.min(top,bottom), Math.min(left,right)) + " ");
-          }
-          System.out.println();
-      }
+        int center = 1;
+        for(int i = 1; center <= 2*n-1; center++){
+            int num = n;
+            for(int j = 1; j <= 2*n-1; j++){
+                System.out.print(num + " ");
+                if( j < i){
+                    num--;
+                }
+                if((j >= (2*n-1)-i+1) && j > n-1){
+                    num++;
+                }
+            }
+            System.out.println();
+                
+            if(center > n-1){
+                i--;
+            }
+            else{
+                i++;
+            }
+        }
     
     }
 }
