6f825a1b2484a3dd33bdf60cbaf5ae1cdb6271e3
aniketchavan6081 <123833092+aniketchavan6081@users.noreply.github.com>
2023-07-16 00:06:38 +0530

    Time: 115 ms (5.34%), Space: 39.6 MB (77.91%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        // return (int)Math.sqrt(x);
+        double i=0;
+        while(i*i<=x)
+        {
+            
+            if( (i*i) <= x && (i+1)*(i+1) > x)
+                return (int)i;
+                i++;
+        }
+        return 0;
     }
 }
