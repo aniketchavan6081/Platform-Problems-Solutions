6b987358b52df6ed210a967671ae71e92d1a4b0b
aniketchavan6081 <123833092+aniketchavan6081@users.noreply.github.com>
2023-07-26 00:16:22 +0530

    Time: 1 ms (66.30%), Space: 40.7 MB (47.85%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,16 +1,19 @@
 class Solution {
     public String interpret(String command) {
-        String str = "";
-        for(int i = 0; i < command.length(); i++){
-            if(command.charAt(i)=='G'){
-                str = str + "G";
-            }else if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
-                str = str + "o";
-            }
-            else if(command.charAt(i) == 'a' && command.charAt(i+1) == 'l'){
-                str = str + "al";
-            }
-        }
-        return str;
+        // String str = "";
+        // for(int i = 0; i < command.length(); i++){
+        //     if(command.charAt(i)=='G'){
+        //         str = str + "G";
+        //     }else if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+        //         str = str + "o";
+        //     }
+        //     else if(command.charAt(i) == 'a' && command.charAt(i+1) == 'l'){
+        //         str = str + "al";
+        //     }
+        // }
+        // return str;
+        
+        
+        return command.replace("()", "o").replace("(al)", "al");
     }
 }
