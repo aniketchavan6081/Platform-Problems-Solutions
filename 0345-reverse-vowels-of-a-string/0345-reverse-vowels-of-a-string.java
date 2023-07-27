4cc1fe0384c5f39000643c1e16922f78af3aea08
aniketchavan6081 <123833092+aniketchavan6081@users.noreply.github.com>
2023-07-27 12:56:15 +0530

    Time: 66 ms (18.93%), Space: 45.4 MB (8.77%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String reverseVowels(String s) {
+        String str = "";
+        int j = 0;
+        char a[] = s.toCharArray();
+        for(int i = 0; i < a.length; i++){
+            if(a[i] == 'a' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' || a[i] == 'e' || a[i] == 'A' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U' || a[i] == 'E'){
+                str += a[i]; 
+                j++;
+            }
+        }
+        
+        StringBuffer sb = new StringBuffer(str);
+        str = sb.reverse().toString();
+        
+        int k = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(a[i] == 'a' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u' || a[i] == 'e' || a[i] == 'A' || a[i] == 'I' || a[i] == 'O' || a[i] == 'U' || a[i] == 'E'){
+                a[i] = str.charAt(k);
+                k++;
+            }
+            if(j==k){
+                break;
+            }
+        }
+        s = new String(a);
+        return s;
+    }
+}
