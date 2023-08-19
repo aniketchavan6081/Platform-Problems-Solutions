4432119363bacaa4be80743f6ce1c11404b3b194
aniketchavan6081 <123833092+aniketchavan6081@users.noreply.github.com>
2023-08-19 23:43:26 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (83.56%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -1,19 +1,31 @@
 class Solution {
     public int dominantIndex(int[] nums) {
-        int len = nums.length;
-        int arr[] = new int[nums.length];
-        for(int i = 0; i < len; i++){
-            arr[i] = nums[i];
+        // int len = nums.length;
+        // int arr[] = new int[nums.length];
+        // for(int i = 0; i < len; i++){
+        //     arr[i] = nums[i];
+        // }
+        // Arrays.sort(arr);
+        // if(arr[len-1] >= arr[len-2]*2){
+        //     for(int i = 0; i < len; i++){
+        //         // System.out.println(nums[i]);
+        //         if(arr[len-1]==nums[i]){
+        //             return i;
+        //         }
+        //     }
+        // }
+        // return -1;
+        int result = Integer.MIN_VALUE;
+        int tmp = 0;
+        int ans = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] > result) {
+                tmp = result;
+                result = nums[i];
+                ans = i;
+            } else if (nums[i] > tmp) 
+                tmp = nums[i];
         }
-        Arrays.sort(arr);
-        if(arr[len-1] >= arr[len-2]*2){
-            for(int i = 0; i < len; i++){
-                // System.out.println(nums[i]);
-                if(arr[len-1]==nums[i]){
-                    return i;
-                }
-            }
-        }
-        return -1;
+        return result >= tmp * 2 ? ans : -1;
     }
 }
