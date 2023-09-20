d3d66f72a3cec36de9a9e3c92df56134d67d996e
aniketchavan6081 <123833092+aniketchavan6081@users.noreply.github.com>
2023-09-20 22:27:43 +0530

    Time: 0 ms (100.00%), Space: 49.6 MB (78.95%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int i = 0;
@@ -8,5 +9,29 @@ class Solution {
             i += 2;
         }
         return nums[nums.length - 1];
+    }
+}
+*/
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid % 2 == 1) {
+                mid--; // Ensure mid is an even index
+            }
+
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid; // The non-duplicate is on the left side
+            } else {
+                left = mid + 2; // The non-duplicate is on the right side
+            }
+        }
+
+        return nums[left];
     }
 }
