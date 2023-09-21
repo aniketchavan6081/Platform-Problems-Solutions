e504b608fdff9202734d00c0607c16999ae5b95c
aniketchavan6081 <123833092+aniketchavan6081@users.noreply.github.com>
2023-09-21 19:27:34 +0530

    Time: 13 ms (89.67%), Space: 43.7 MB (61.99%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -7,6 +7,8 @@ class Solution {
         int currE = -1;
         int currCnt = 0;
 
+
+        
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] % 2 == 0) {
                 if(nums[i] == currE) {
